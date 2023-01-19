74ffa702afbf5f4b764a73f4d3a7d313ba2541c4
gstv1478 <gstv1478@gmail.com>
2023-01-19 18:45:00 -0300

    terceiro commit

--- app.js
+++ app.js
@@ -18,3 +18,36 @@ navToggle.addEventListener("click", function() {
         linksContainer.style.height = 0;
     }
 });
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+
+// fixed navbar
+window.addEventListener("scroll", function(){
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if (scrollHeight > navHeight) {
+        navbar.classList.add("fixed-nav");
+    } else {
+        navbar.classList.remove("fixed-nav");
+    }
+
+    if(scrollHeight > 500) {
+        topLink.classList.add('show-link');
+    } else {
+        topLink.classList.remove("show-link");
+    }
+});
+//smooth scroll
+//select links
+const scrollLinks = document.querySelectorAll(".scroll-link")
+
+scrollLinks.forEach(function (link) {
+    link.addEventListener("click", function(e) {
+        //prevent default
+        e.preventDefault();
+        //navigate to specific spot
+        const id = e.currentTarget.getAttribute("href");
+        console.log(id);
+    })
+    
+})
