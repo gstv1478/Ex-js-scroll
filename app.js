f4f2e767e4392a9ecd654293d1bf2c712300878b
gstv1478 <gstv1478@gmail.com>
2023-01-21 13:20:07 -0300

    quarto commit

--- app.js
+++ app.js
@@ -46,8 +46,26 @@ scrollLinks.forEach(function (link) {
         //prevent default
         e.preventDefault();
         //navigate to specific spot
-        const id = e.currentTarget.getAttribute("href");
-        console.log(id);
-    })
+        const id = e.currentTarget.getAttribute("href").slice(1);
+        const element = document.getElementById(id);
+        //calculate the heights
+        const navHeight = navbar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const fixedNav = navbar.classList.contains("fixed-nav");
+        let position = element.offsetTop - navHeight;
+
+        if (!fixedNav) {
+            position = position - navHeight;
+        }
+        if (navHeight > 82) {
+            position = position + containerHeight;
+        }
+
+        window.scrollTo({
+            left: 0,
+            top: position,
+        });
+        linksContainer.style.height = 0;
+    });
     
-})
+});
